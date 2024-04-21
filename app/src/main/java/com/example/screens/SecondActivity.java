1c13daabf13217165a911c94bb484f8fca86cfcd
b23sebth <b23sebth@student.his.se>
2024-04-21 19:57:01 +0200

    Added button with intent to return to main activity.

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,9 +1,9 @@
 package com.example.screens;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.example.screens.R;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +12,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button button = findViewById(R.id.button_secondactivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
